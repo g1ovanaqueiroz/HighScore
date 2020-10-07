6c0a35f6586e34c3810445f741483a98c1d965de
giovanabritooliveira <giovana.oliveira@ccc.ufcg.edu.br>
2020-10-07 10:45:53 -0300

    repa-resumo

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,16 @@
 import React, {Component} from 'react';
 import './App.css';
 
-import Comentario from './components/Comentario/Comentario'
 import Footer from './components/Footer/Footer'
 import Header from './components/Header/Header'
+import PecaResumo from './components/Peca/PecaResumo'
+
 
 function App() {
   return (
-    <div className="App">
-      <Header></Header>
+    <div className="peca-resumo">
+      <header><Header></Header></header>
+      <PecaResumo></PecaResumo>
       <Footer></Footer>
     </div>
   );
---
+++ frontend/src/components/Peca/PecaResumo.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './PecaResumo.css'
+
+export default function PecaResumo() {
+  return (
+    <div className='peca-resumo'>
+      <a href="./">
+        <fieldset>
+          <h3>Für Elise</h3>
+          <b>#piano #iniciante #ludwigVanBeethoven #beethoven #solo #classicismo #romantismo</b>
+        </fieldset>
+      </a>
+    </div>
+  );
+}

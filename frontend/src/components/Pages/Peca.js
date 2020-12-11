4d8e3594df0915d34758c13e88cf828a48daa740
giovanabritooliveira <giovana.oliveira@ccc.ufcg.edu.br>
2020-12-11 07:53:56 -0300

    finalizando esqueleto

--- frontend/src/components/Pages/Peca.js
+++ frontend/src/components/Pages/Peca.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Header from '../Header/Header'
 import Footer from '../Footer/Footer'
 import Descricao from '../Peca/Descricao/Descricao'
-import AdicionaDiscussao from '../Peca/AdicionaDiscussao/adicionaDiscussao'
+import AdicionaDiscussao from '../Discussao/AdicionaDiscussao/adicionaDiscussao'
 import DiscussaoResumo from '../Discussao/DiscussaoResumo'
 import './Peca.css'
 

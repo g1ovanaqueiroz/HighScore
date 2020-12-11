2839c958bf39c444913a07a21412e00e8670214d
giovanabritooliveira <giovana.oliveira@ccc.ufcg.edu.br>
2020-12-11 14:51:22 -0300

    refazendo backend

---
+++ backend/src/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.get('/', (req, res)  => {
+  res.send('OK');
+})
+
+app.listen(8082, function(){
+  console.log('server on...')
+});

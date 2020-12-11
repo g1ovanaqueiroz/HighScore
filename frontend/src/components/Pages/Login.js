5b4196df70a5db3a3e3e56f227d854748cfe0c0f
giovanabritooliveira <giovana.oliveira@ccc.ufcg.edu.br>
2020-12-10 23:46:21 -0300

    login

--- frontend/src/components/Pages/Login.js
+++ frontend/src/components/Pages/Login.js
@@ -1,10 +1,47 @@
 import React from 'react'
 import LoginHeader from '../Header/LoginHeader'
+import {ErrorMessage, Formik, Form, Field} from 'formik'
+import './Login.css'
+import * as yup from 'yup'
 
 export default function Login() {
+  const handleSubmit = values => console.log(values)
+  const validations = yup.object().shape({
+    email: yup.string().email().required(),
+    password: yup.string().min(8).required()
+  })
+
   return ([
     <div className="login">
       <LoginHeader/>
+      <Formik 
+      initialValues={{}} 
+      onSubmit={handleSubmit} 
+      validationSchema={validations}>
+        <Form className="Form">
+          <div className="Form-Group">
+            <p><b>Email:</b></p>
+            <Field name="email"
+            className="Form-Field"
+            />
+            <ErrorMessage
+            component="span"
+            name="email"
+            className="Form-Error"
+            />
+            <p><b>Senha:</b></p>
+            <Field name="password"
+            className="Form-Field"
+            />
+            <ErrorMessage
+            component="span"
+            name="password"
+            className="Form-Error"
+            />
+            <button className="Form-btn" type="submit">Login</button>
+          </div>
+        </Form>
+      </Formik>
     </div>
   ])
 }
